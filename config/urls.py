0b48352369ca1774cb421014ba3e034e0283708e
shuji watanabe <rinkioutouhenmonzizai@yahoo.co.jp>
2020-03-16 12:23:50 +0900

    firstcommit

---
+++ config/urls.py
@@ -0,0 +1,29 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+
+
+def profile_view(request):
+    return HttpResponse("Welcome %s <br> <a href='/accounts/logout/'>Logout</a>" % request.user)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('accounts/profile/', profile_view)
+]
